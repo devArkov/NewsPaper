aa76494de3dbbb6a4b3ce7e5e68b72394e5ea930
Alexandr Korobov <devarko@sapo.pt>
2022-12-17 20:55:32 +0300

    Login page & url

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,4 +10,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
+    # Django auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('accounts/profile', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='profile'),
 ]
