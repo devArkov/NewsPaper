c93a5755da22e72966f8a991b3dafa283b451ea9
Alexandr Korobov <devarko@sapo.pt>
2022-12-17 20:32:15 +0300

    Navbar highlights active links

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about/', views.about, name='home'),
-    path('contact/', views.contact, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
